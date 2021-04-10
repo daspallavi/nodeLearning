3d4ccf8e54bafa2859b8ea6983818dc916d43342
Pallavi Das <pallavids92@gmail.com>
2021-04-10 16:25:32 +0530

    EJS layout
    EJS layout

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// set global configuration values : views, view engine etc
-
-//this is for handlbar
-//app.engine('hbs', expressHbs());
-
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -21,10 +16,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-console.log('app');
+
 app.use((req, res, next) => {
-    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.render("404", { pageTitle: '404!' })
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,22 @@
 const path = require('path');
+
 const express = require('express');
+
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('from shop.js', adminData.products);
   const products = adminData.products;
-  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-
-  //nodejs method render used to render template shop.pud. extension is not needed to mention as in app.js
-  // default template engine is already set in global configuration app.set()
-  // render method also helps to send data attached to it in form of object to set in synamic content
-  // these data will be available in shop.pug file
-  res.render('shop', { prods: products, docTitle: 'Shop' })
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
