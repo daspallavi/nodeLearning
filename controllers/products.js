35607cacea836de9e7c5caa2a2ccc8c1f8f8815e
Pallavi Das <pallavids92@gmail.com>
2021-06-03 23:49:35 +0530

    controller changes
    controller changes

--- controllers/products.js
+++ controllers/products.js
@@ -1,24 +1,9 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-};
-
-exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
-  product.save();
-  res.redirect('/');
-};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
@@ -28,3 +13,4 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+å
